6ba9e07be1e16fead81300d8c4415907d8e93ef4
Landagritech <104423152+Landagritech@users.noreply.github.com>
2022-04-30 11:30:34 +0200

    New Features : * Project IDentityServer * Token consuming * UI Project and template intergration

---
+++ IDC/Constants.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace IDC
+{
+    public static class Constants
+    {
+        public static string SecretKey = "MyLANDAGRITECH+Houssemeddine+1986+Medenine+Agro";
+        public static string APIName = "LANDO";
+        public static string APIURL = "https://localhost:35131";
+        public static string Role1 = "LANDO.SuperAdmin";
+        public static string Role2 = "LANDO.SuperInternal";
+        public static string Role3 = "LANDO.Internal";
+        public static string Role4 = "LANDO.Partner";
+        public static string Role5 = "LANDO.Customer";
+    }
+}
---
+++ IDC/IdentityConfiguration.cs
@@ -0,0 +1,73 @@
+﻿
+using System.Collections.Generic;
+using System.Security.Claims;
+using IdentityModel;
+using IdentityServer4.Models;
+using IdentityServer4.Test;
+
+namespace IDC
+{
+    public class IdentityConfiguration
+    {
+        public static List<TestUser> TestUsers =>
+        new List<TestUser>
+         {
+            new TestUser
+            {
+                SubjectId = "00001",
+                Username = "admin",
+                Password = "admin",
+                Claims =
+                {
+                    new Claim(JwtClaimTypes.Name, "Houssemeddine HENCHIR"),
+                    new Claim(JwtClaimTypes.GivenName, "Houssem"),
+                    new Claim(JwtClaimTypes.FamilyName, "HENCHIR"),
+                }
+            }
+         };
+
+        public static IEnumerable<IdentityResource> IdentityResources =>
+        new IdentityResource[]
+            {
+                new IdentityResources.OpenId(),
+                new IdentityResources.Profile(),
+            };
+
+
+        public static IEnumerable<ApiScope> ApiScopes =>
+            new ApiScope[]
+            {
+                new ApiScope("LANDO.SuperAdmin"),
+                new ApiScope("LANDO.Customer"),
+                new ApiScope("LANDO.Partner"),
+                new ApiScope("LANDO.Internal"),
+                new ApiScope("LANDO.SuperInternal"),
+            };
+
+        public static IEnumerable<ApiResource> ApiResources =>
+            new ApiResource[]
+            {
+                new ApiResource(Constants.APIName)
+                {
+                    Scopes = new List<string>{ Constants.Role1, Constants.Role2,Constants.Role3,Constants.Role4,Constants.Role5},
+                    ApiSecrets = new List<Secret>{ new Secret(Constants.SecretKey.Sha256()) }
+                }
+            };
+
+        public static IEnumerable<Client> Clients =>
+            new Client[]
+            {
+                new Client
+                {
+                    ClientId = "cwm.client",
+                    ClientName = "Client Credentials Client",
+                    AllowedGrantTypes = GrantTypes.ClientCredentials,
+                    ClientSecrets = { new Secret(Constants.SecretKey.Sha256()) },
+                    AllowedScopes = { Constants.Role1 }
+                },
+            };  
+    }
+
+
+    
+}
